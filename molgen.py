d362a75563c9de71a85ea625b3165ef6f7d8966f
QHwan <poter1026@gmail.com>
2019-06-23 21:43:37 +0900

    graphene module

--- molgen.py
+++ molgen.py
@@ -28,7 +28,6 @@ class Residue:
                         y = ref_y + j*dy
                         z = ref_z + k*dz
                         atom = Atom(atom_name, [x, y, z])
-                        print(x, y, z)
                         self.atom_vec.append(atom)
 
 
@@ -69,27 +68,35 @@ def write_gro(ofilename, total):
                 ofile.write("%5d%-5s%5s%5d%8.3f%8.3f%8.3f" % (res_num, res_name, atom_name, atom_num, x, y, z))
                 ofile.write("\n")
 
-        ofile.write("{}\t{}\t{}".format(box[0], box[1], box[2]))
+        ofile.write("%.3f\t%.3f\t%.3f" % (box[0], box[1], box[2]))
         ofile.write("\n")
+
+def make_graphene(total, x, y, set_box=True):
+    GRA = Residue("GRA")
+
+    ccbond = 0.142
+    dx=ccbond*math.cos(30*(math.pi/180.0))
+    dy=ccbond*math.sin(30*(math.pi/180.0))  
+    C1 = Atom("C", [0., 0., 0.])
+    C2 = Atom("C", [-1*dx, dy, 0.])
+    C3 = Atom("C", [-1*dx, dy+ccbond, 0.])
+    C4 = Atom("C", [0, 2*dy+ccbond, 0.])
+    GRA.add_atom(C1)
+    GRA.add_atom(C2)
+    GRA.add_atom(C3)
+    GRA.add_atom(C4)
+    nx = int(x/(2*dx))
+    ny = int(y/(2*dy+2*ccbond))
+    GRA.make_periodic(nx=nx, ny=ny, nz=1, dx=2*dx, dy=2*dy+2*ccbond, dz=0)
+    tot.add_residue(GRA)
+
+    if set_box == True:
+        tot.box[0] = nx*2*dx
+        tot.box[1] = ny*2*(dy+ccbond)
+
 
 box = [10, 10, 10]
 tot = Total(box)
-
-GRA = Residue("GRA")
-
-ccbond = 0.142
-dx=ccbond*math.cos(30*(math.pi/180.0))
-dy=ccbond*math.sin(30*(math.pi/180.0))  
-C1 = Atom("C", [0., 0., 0.])
-C2 = Atom("C", [-1*dx, dy, 0.])
-C3 = Atom("C", [-1*dx, dy+ccbond, 0.])
-C4 = Atom("C", [0, 2*dy+ccbond, 0.])
-GRA.add_atom(C1)
-GRA.add_atom(C2)
-GRA.add_atom(C3)
-GRA.add_atom(C4)
-GRA.make_periodic(nx=5, ny=5, nz=1, dx=2*dx, dy=2*dy+2*ccbond, dz=0)
-tot.add_residue(GRA)
-
+make_graphene(tot, 5, 5)
 ofilename = "test.gro"
 write_gro(ofilename, tot)
